1d0f858c9cb1fad4e29a5782c3767f0c36f8908e
MAHAK0804 <2021pietcsmahak100@poornima.org>
2025-07-08 12:33:58 +0530

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 // Configure CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // your frontend URL
+    origin: "*",
     credentials: true,
   })
 );
