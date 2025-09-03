9b31c3dfb4c8c1fa5f1c574f3e61e4bde157db0f
MAHAK0804 <2021pietcsmahak100@poornima.org>
2025-09-03 12:41:40 +0530

    explore/sounds

--- models/sound.js
+++ models/sound.js
@@ -5,7 +5,7 @@ const soundSchema = new mongoose.Schema(
     title: { type: String, required: true },
     url: { type: String, required: true }, // S3 audio URL
     key: { type: String, required: true }, // S3 audio key
-    image: { type: String, required: true }, // S3 image URL
+    image: { type: String, required: false }, // S3 image URL
     imageKey: { type: String, required: true }, // S3 image key
   },
   { timestamps: true }
